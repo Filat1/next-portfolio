11dc15336dbb88ad568487de9cb51565efaf2e2f
Filat1 <sergeyffilat@gmail.com>
2020-01-27 12:10:12 -0500

    add now.json and scripts

--- pages/post.js
+++ pages/post.js
@@ -3,7 +3,7 @@ import { withRouter } from 'next/router'
 
 const Post = ({ router }) => (
   <Layout title={router.query.title} >
-    <p> Lorem ipsum dolor, sit amet consectetur adipisicing
+    <p style={{ width: "80vw" }} > Lorem ipsum dolor, sit amet consectetur adipisicing
      elit. Necessitatibus, ab. Quibusdam at debitis voluptatum
       consectetur laborum officiis accusamus deleniti, ducimus
       assumenda eligendi voluptas harum expedita ut minima eum
